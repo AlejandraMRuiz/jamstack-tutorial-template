b4f89702b7918d9eb525ada1f8de85c7c28be23d
Morgan Richardson <morganrmarie@gmail.com>
2019-12-19 19:30:09 -0600

    Adding files

---
+++ gatsby-tutorial-final/src/components/individualBlogpost.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import '../styles/individual-blogpost.scss'
+
+const IndividualBlogpost = ({data}) => {
+    const tags = data.tagList;
+
+    const tagList = tags.map((tag) =>
+        <a href={ tag.url }>
+            <li key={ tag.name }>
+                { tag.name }
+            </li>
+        </a>
+    )
+
+    return (
+        <div className="blogpost-container">
+            <div className="blogpost-image"></div>
+            <div className="blogpost-title">
+                <p>{ data.title }</p>
+            </div>
+            <div className="blogpost-summary">
+                <p>{ data.summary }</p> 
+            </div>
+            <ul className="tag-list">{tagList}</ul>
+            <div className="blogpost-date">
+                <p>{ data.date }</p> 
+            </div>
+        </div>
+    );
+}
+
+export default IndividualBlogpost
