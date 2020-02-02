583eee0be9e9afa2133cbf76e51de57afb57ee6d
Morgan Richardson <morganrmarie@gmail.com>
2020-02-01 19:14:00 -0600

    Updating all files with prettier

--- gatsby-tutorial-starter-template/src/components/links.js
+++ gatsby-tutorial-starter-template/src/components/links.js
@@ -3,19 +3,15 @@ import React from 'react'
 import '../styles/links.scss'
 
 const Links = (props) => {
-    const links = props.links;
+  const links = props.links
 
-    const linkList = links.map((link) => 
-        <a href="https://www.instagram.com/morgan.codes">
-            <li key={link.toString()}>
-                {link}
-            </li>
-        </a>
-    );
+  const linkList = links.map((link) => (
+    <a href='https://www.instagram.com/morgan.codes'>
+      <li key={link.toString()}>{link}</li>
+    </a>
+  ))
 
-    return (
-        <ul className="linkList">{linkList}</ul>
-    );
+  return <ul className='linkList'>{linkList}</ul>
 }
 
 export default Links
--- gatsby-tutorial-starter-template/src/pages/index.js
+++ gatsby-tutorial-starter-template/src/pages/index.js
@@ -1,8 +1,8 @@
-import React from "react"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Links from "../components/links"
-import IndividualBlogpost from "../components/individualBlogpost"
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Links from '../components/links'
+import IndividualBlogpost from '../components/individualBlogpost'
 
 import '../styles/index.scss'
 
@@ -10,60 +10,64 @@ import productivity from '../images/productivity.jpg'
 import dailyCarry from '../images/dailycarry.jpg'
 import jamstack from '../images/jamstack.jpg'
 
-const links = ['All', 'Tutorials', 'Lifestyle', 'Personal', 'Resources'];
-const blogpostInformation = 
-  [
-    {
-      tagList: [
-        {
-          name: 'Tutorials',
-          url: 'https://www.instagram.com/morgan.codes'
-        }
-      ],
-      title: 'JAMSTACK intro',
-      summary: 'Create a blazing fast static blog website with JAMstack using Gatsby, Contentful, and Netlify.',
-      date: 'January 1st, 2020',
-      imgHeader: jamstack
-    },
-    {
-      tagList: [
-        {
-          name: 'Lifestyle',
-          url: 'https://www.instagram.com/morgan.codes'
-        },
-        {
-          name: 'Personal',
-          url: 'https://www.instagram.com/morgan.codes'
-        }
-      ],
-      title: 'What\'s In My Bag?',
-      summary: 'Sharing my daily necessities as a Software Developer that I carry in my ISM backpack everyday.',
-      date: 'February 3rd, 2020',
-      imgHeader: dailyCarry
-    },
-    {
-      tagList: [{
+const links = ['All', 'Tutorials', 'Lifestyle', 'Personal', 'Resources']
+const blogpostInformation = [
+  {
+    tagList: [
+      {
+        name: 'Tutorials',
+        url: 'https://www.instagram.com/morgan.codes',
+      },
+    ],
+    title: 'JAMSTACK intro',
+    summary:
+      'Create a blazing fast static blog website with JAMstack using Gatsby, Contentful, and Netlify.',
+    date: 'January 1st, 2020',
+    imgHeader: jamstack,
+  },
+  {
+    tagList: [
+      {
+        name: 'Lifestyle',
+        url: 'https://www.instagram.com/morgan.codes',
+      },
+      {
+        name: 'Personal',
+        url: 'https://www.instagram.com/morgan.codes',
+      },
+    ],
+    title: "What's In My Bag?",
+    summary:
+      'Sharing my daily necessities as a Software Developer that I carry in my ISM backpack everyday.',
+    date: 'February 3rd, 2020',
+    imgHeader: dailyCarry,
+  },
+  {
+    tagList: [
+      {
         name: 'Resources',
-        url: 'https://www.instagram.com/morgan.codes'
-      }],
-      title: 'Improve Your Workflow',
-      summary: 'Drastically improve your workflow and increase your productivity with these VSCode extensions.',
-      date: 'March 4th, 2020',
-      imgHeader: productivity
-    },
-  ];
+        url: 'https://www.instagram.com/morgan.codes',
+      },
+    ],
+    title: 'Improve Your Workflow',
+    summary:
+      'Drastically improve your workflow and increase your productivity with these VSCode extensions.',
+    date: 'March 4th, 2020',
+    imgHeader: productivity,
+  },
+]
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="JAMstack Template" />
-    <div className="blogposts-main">
-      <h1 className="header">Blogposts</h1>
-      <div className="divider"></div>
-      <Links links={links} className="linkList"/>
-      <div className="blogposts-content">
-        {blogpostInformation.map((singleBlogpostInfo) =>
-          <IndividualBlogpost data={ singleBlogpostInfo }/>  
-        )}
+    <SEO title='JAMstack Template' />
+    <div className='blogposts-main'>
+      <h1 className='header'>Blogposts</h1>
+      <div className='divider'></div>
+      <Links links={links} className='linkList' />
+      <div className='blogposts-content'>
+        {blogpostInformation.map((singleBlogpostInfo) => (
+          <IndividualBlogpost data={singleBlogpostInfo} />
+        ))}
       </div>
     </div>
   </Layout>
